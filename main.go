bcf4a3bb4543bb8cf950064f6129d0fde969376b
Mohit Hooda <mohit@Mohits-MacBook-Air.local>
2022-11-30 15:19:41 +0530

    Structs added

---
+++ main.go
@@ -0,0 +1,27 @@
+ package main
+
+ import (
+	 "fmt"
+	 "log"
+	 "encoding/json"
+	 "math/rand"
+	 "net/http"
+	 "strconv"
+	 "github.com/gorilla/mux"
+ )
+
+ type Movie struct{
+	 ID string `json:"id"`
+	 Isbn string `json:"isbn"`
+	 Title string `json:"title"`
+	 Director *Director `json:"director"`	
+	}
+
+ type Director{
+	 Firstname string `json:"firstname"`
+	 Lastname string `json:"lastname"`
+ }
+
+ func main(){
+	 
+ }
