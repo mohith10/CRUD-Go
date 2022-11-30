72a2e5f8e76936c1b4f56e5866982a96b1dc9107
Mohit Hooda <mohit@Mohits-MacBook-Air.local>
2022-11-30 15:25:56 +0530

    Routes added

--- main.go
+++ main.go
@@ -1,27 +1,36 @@
- package main
+package main
 
- import (
-	 "fmt"
-	 "log"
-	 "encoding/json"
-	 "math/rand"
-	 "net/http"
-	 "strconv"
-	 "github.com/gorilla/mux"
- )
+import (
+	"fmt"
+	"log"
+	"net/http"
 
- type Movie struct{
-	 ID string `json:"id"`
-	 Isbn string `json:"isbn"`
-	 Title string `json:"title"`
-	 Director *Director `json:"director"`	
-	}
+	"github.com/gorilla/mux"
+)
 
- type Director{
-	 Firstname string `json:"firstname"`
-	 Lastname string `json:"lastname"`
- }
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
 
- func main(){
-	 
- }
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovies).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movie/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movie/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting servier at 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
